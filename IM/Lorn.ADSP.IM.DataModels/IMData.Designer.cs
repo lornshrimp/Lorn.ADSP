61b338ba5be3264b6d4fca04e06c78ced036481f
独孤大虾 <lornshrimp@outlook.com>
2015-05-27 16:31:58 +0800

    update to .Net 4.6

--- IM/Lorn.ADSP.IM.DataModels/IMData.Designer.cs
+++ IM/Lorn.ADSP.IM.DataModels/IMData.Designer.cs
@@ -1,4 +1,4 @@
-﻿// 为模型“C:\Users\兵\SkyDrive\Visual Studio\Projects\Lorn.ADSP\IM\Lorn.ADSP.IM.DataModels\IMData.edmx”启用了 T4 代码生成。
+﻿// 为模型“C:\Users\Bill\OneDrive\Visual Studio\Projects\Lorn.ADSP\IM\Lorn.ADSP.IM.DataModels\IMData.edmx”启用了 T4 代码生成。
 // 要启用旧代码生成功能，请将“代码生成策略”设计器属性的值
 // 更改为“旧的 ObjectContext”。当在设计器中打开该模型时，此属性会出现在
 // “属性”窗口中。
