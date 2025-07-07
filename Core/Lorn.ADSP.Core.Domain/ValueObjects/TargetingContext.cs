bf681aaa5fab2e13e5d5cdbba41dbc5210227441
lornshrimp <lornshrimp.pm@outlook.com>
2025-07-08 00:33:04 +0800

    feat: 添加定向策略、人口属性定向、行为定向及相关用户行为记录类

---
+++ Core/Lorn.ADSP.Core.Domain/ValueObjects/TargetingContext.cs
@@ -0,0 +1,35 @@
+﻿using Lorn.ADSP.Core.Domain.Entities;
+
+namespace Lorn.ADSP.Core.Domain.ValueObjects
+{
+    /// <summary>
+    /// 定向上下文
+    /// </summary>
+    public class TargetingContext
+    {
+        /// <summary>
+        /// 地理位置
+        /// </summary>
+        public GeoInfo? GeoLocation { get; set; }
+
+        /// <summary>
+        /// 用户画像
+        /// </summary>
+        public UserProfile? UserProfile { get; set; }
+
+        /// <summary>
+        /// 设备信息
+        /// </summary>
+        public DeviceInfo? DeviceInfo { get; set; }
+
+        /// <summary>
+        /// 请求时间
+        /// </summary>
+        public DateTime RequestTime { get; set; } = DateTime.UtcNow;
+
+        /// <summary>
+        /// 用户行为
+        /// </summary>
+        public UserBehavior? UserBehavior { get; set; }
+    }
+}
